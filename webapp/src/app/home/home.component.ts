c2dadcf11e96bc12af7dfa653551b4b8a254b4b2
Martin Jamszolik <martin.jozef@gmail.com>
2021-08-14 00:10:32 -0400

    Update linting

--- webapp/src/app/home/home.component.ts
+++ webapp/src/app/home/home.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
